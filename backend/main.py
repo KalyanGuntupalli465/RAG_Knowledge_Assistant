c65e977a2ebf953a548396c0528fcc9be0db4c94
gmk <gmk@example.com>
2026-05-14 10:16:23 -0500

    fixed deploying issues

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://*.vercel.app",
+        "https://rag-knowledge-assistant-two.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
